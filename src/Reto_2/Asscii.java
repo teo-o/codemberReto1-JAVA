579a8c05600b886a5a37b9f5883cbf2781411ec9
teo-o <mateoorozco313199@correo.itm.edu.co>
2022-11-19 22:57:23 -0500

    update reto2 Codember

--- src/Reto_2/Asscii.java
+++ src/Reto_2/Asscii.java
@@ -8,7 +8,17 @@ public class Asscii {
     List<HashMap> lettersAndNumbers = new ArrayList<>();
 
     public void completeList(){
+        for (int i = 65; i < 91; i++) {
+            HashMap letter = new HashMap();
+            letter.put(i, (char)i);
+            lettersAndNumbers.add(letter);
+        }
         for (int i = 97; i < 123; i++) {
+            HashMap letter = new HashMap();
+            letter.put(i, (char)i);
+            lettersAndNumbers.add(letter);
+        }
+        for (int i = 48; i < 58; i++) {
             HashMap letter = new HashMap();
             letter.put(i, (char)i);
             lettersAndNumbers.add(letter);
--- src/Reto_2/main.java
+++ src/Reto_2/main.java
@@ -7,7 +7,7 @@ public class main {
     public static void main(String[] args) {
         Asscii asscii = new Asscii();
         asscii.completeList();
-        descifrarCodigo("11610497110107115 102111114 11210897121105110103 9911110010110998101114 11210810197115101 11510497114101", asscii.getLettersAndNumbers());
+        descifrarCodigo("83101 113117105101110 101114101115 84101 9911111011112299111 84117 110111 109101 9911111011199101115 97 109105 84101 101115116111121 1119811510111411897110100111 84101 101115116111121 115105103117105101110100111 81117105101114101115 10611710397114 7411710110397 99111110109105103111 8697108101 8697109111115 97 10611710397114 691061019911711697 101115116101 9911110997110100111 101110 10897 11610111410910511097108 11511798109105116 116561181061045651505752561029911097108", asscii.getLettersAndNumbers());
     }
 
     public static void descifrarCodigo(String codigo, List<HashMap> lettersAndNumbers){
@@ -18,7 +18,8 @@ public class main {
             int a=0;
             for( int j = 0; j < palabra.length()-2; j+=a) {
                 String numeroSTR = palabra.charAt(j) + "" + palabra.charAt(j + 1);
-                if (numeroSTR.equals("97") || numeroSTR.equals("98") || numeroSTR.equals("99")) {
+                int numero1 = Integer.parseInt(numeroSTR);
+                if ((numero1 <= 99 && numero1 >= 65) ||(numero1 >= 48 && numero1 <= 57)) {
                     a = 2;
                 } else {
                     numeroSTR = palabra.charAt(j) + "" + palabra.charAt(j + 1) + "" + palabra.charAt(j + 2);
